4f083b780091fad571a782a45e315ca22bf1c8f5
Sergey <sergeylapitsky@yandex.ru>
2024-04-17 14:12:34 +0300

    add lesson3:task1

---
+++ CSharp/lessons/lesson3/task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Вычислить значение формулы a * b / c + d, где
+// a, b, c, d - некоторые целые числа. Решение
+// оформить в виде функции.
+
+double CalculateFormul(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    int denomenator = c + d;
+    double result = numenator / denomenator;
+    return result;
+}
+Console.WriteLine(CalculateFormul(1, 2, 3, 4));
