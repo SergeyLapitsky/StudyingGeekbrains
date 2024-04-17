ab7eba896c4da0c83ed03c9f767164d2535e3e05
Sergey <sergeylapitsky@yandex.ru>
2024-04-17 11:50:53 +0300

    add branch introduction_programming_languages

---
+++ CSharp/PracticalExercises/task1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 1: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+void CheckIfDevides(int num)
+{
+    if(num % 7 == 0 && num % 23 == 0)
+    {
+        System.Console.WriteLine("Number multiple 7 and 23");
+    }
+    else 
+    {
+        System.Console.WriteLine("Number not multiple 7 and 23");
+    }
+}
+
+Console.Write("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+CheckIfDevides(num);
---
+++ CSharp/PracticalExercises/task2/Program.cs
@@ -0,0 +1,38 @@
+﻿//Задача 2: Напишите программу, которая принимает на вход координаты точки (X и Y),
+//причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, в которой находится эта точка.
+
+void DefinitionOfCoordinateQuarter (int x, int y)
+{
+    if(x != 0 && y != 0 )
+    {
+        if(x < 0)
+        {
+            if(y < 0)
+            {
+                System.Console.WriteLine("Number is in the Third quarter");
+            }
+            else
+            {
+                System.Console.WriteLine("Number is in the Second quarter");
+            }
+        }
+        else 
+        {
+           if(y < 0)
+            {
+                System.Console.WriteLine("Number is in the Fourth quarter");
+            }
+            else
+            {
+                System.Console.WriteLine("Number is in the First quarter");
+            } 
+        }
+    }
+}
+
+Console.Write("Input x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+DefinitionOfCoordinateQuarter(x, y);
