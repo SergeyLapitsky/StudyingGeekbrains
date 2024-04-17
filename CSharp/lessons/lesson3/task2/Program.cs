96e7fbc750f71c43beaadc713adae1318d6323e4
Sergey <sergeylapitsky@yandex.ru>
2024-04-17 14:40:23 +0300

    add lesson3:task2

---
+++ CSharp/lessons/lesson3/task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Вывод на экран квадратов чисел от 1 до N.
+
+void PrintSquares(int limit)
+{
+    int i = 1;
+    while(i <= limit)
+    {
+        Console.Write($"{i * i} ");
+        i = i + 1;
+    }
+}
+PrintSquares(5);
+PrintSquares(10);
+PrintSquares(15);
