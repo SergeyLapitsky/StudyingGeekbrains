393d1b49574f7ff2d5230dbc7df3f5aa542169ac
Sergey <sergeylapitsky@yandex.ru>
2024-04-18 12:03:02 +0300

    add seminar4:task3

---
+++ CSharp/seminars/task3/Program.cs
@@ -0,0 +1,49 @@
+﻿// Заполните массив на N (вводится с консоли, не более 8)
+// случайных целых чисел от 0 до 9.
+// Сформируйте целое число, которое будет состоять из цифр из
+// массива. Старший разряд числа находится на 0-м индексе,
+// младший – на последнем.
+// Пример
+// [1 3 2 4 2 3] => 132423
+// [2 3 1] => 231
+
+// Генерация массива со случайными числами
+int[] CreateArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+
+int ConvertToNum(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        result = result + array[i] * (int)Math.Pow(10, array.Length - 1 - i);
+    }
+    return result;
+}
+
+System.Console.WriteLine("Input array size: ");
+int size = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input minimal value of arr element: ");
+int min = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input maximal value of arr element: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] array = CreateArray(size, min, max);
+PrintArray(array);
+System.Console.WriteLine(ConvertToNum(array));
