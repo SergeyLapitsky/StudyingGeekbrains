40b75973717d3bc75a3c444844b3b079d8fdb51d
Sergey <sergeylapitsky@yandex.ru>
2024-04-19 12:46:39 +0300

    add lesson4:task1

---
+++ CSharp/lessons/Lesson4/task1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Создать двумерный массив с размерами
+// 3 * 5, состоящий из целых чисел
+// Вывести его элементы на экран
+
+int[,] CreateMatrix(int rowCount, int columsCount)
+{
+    int[,] matrix = new int[rowCount, columsCount];
+    Random rnd = new Random();
+    // Определение количества строк в двумерном массиве
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        // Определение количества столбцов в двумерном массиве
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 11);
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    // Вывод элементов двумерного массива
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        // Вывод одномерного массива
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = CreateMatrix(4, 5);
+ShowMatrix(matrix);
